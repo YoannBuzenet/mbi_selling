26b5290aeede7b08d81ebe142c9ce93d0deb23e4
Yoann <ybuzenet@gmail.com>
2020-09-26 16:21:09 +0200

    PriceGuide Entity

---
+++ migrations/20200926141819-create-price-guide.js
@@ -0,0 +1,97 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface
+      .createTable("PriceGuides", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(function () {
+        queryInterface.bulkInsert("priceguides", [
+          {
+            name: "commander",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "duel",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "legacy",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "modern",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "vintage",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "penny",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "pauper",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "historic",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "pioneer",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "brawl",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "future",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "standard",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "oldschool",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ]);
+      });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("PriceGuides");
+  },
+};
