16c28159a8c0c302833d5a8f136b21180f97304f
Yoann <ybuzenet@gmail.com>
2020-09-26 16:03:21 +0200

    adding snapshop params table first version (still missing price brackets) I changed the order of the migration because put_request has a dpendancy over snapshot_params table

---
+++ migrations/20200926123554-create-snapshot-params.js
@@ -0,0 +1,99 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("snapshot_params", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      shopId: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerSigned: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerMintRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerNearMintRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerGoodRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLightPlayedRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerPlayedRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerPoorRegular: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerMintFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerNearMintFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerGoodFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLightPlayedFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerPlayedFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerPoorFoil: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangGerman: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangSpanish: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangFrench: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangItalian: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangJapanese: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangPortuguese: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangRussian: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangSimplifiedChinese: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangEnglish: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangKorean: {
+        type: Sequelize.INTEGER,
+      },
+      percentPerLangTraditionalChinese: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("snapshot_params");
+  },
+};
--- models/ordercards.js
+++ models/ordercards.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class OrderCards extends Model {
     /**
@@ -12,12 +10,22 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  OrderCards.init({
-    amount: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'OrderCards',
-  });
+  }
+  OrderCards.init(
+    {
+      amount: DataTypes.INTEGER,
+      condition: DataTypes.STRING,
+      lang: DataTypes.INTEGER,
+      isFoil: DataTypes.INTEGER,
+      isSigned: DataTypes.INTEGER,
+      isAltered: DataTypes.INTEGER,
+      isPlayset: DataTypes.INTEGER,
+      idOrder: DataTypes.INTEGER, //TODO check si les relations s'écrivent comme ça
+    },
+    {
+      sequelize,
+      modelName: "OrderCards",
+    }
+  );
   return OrderCards;
 };
