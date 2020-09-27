ba60c4768527203dc1a14a1917ac8a9692933e75
Yoann <ybuzenet@gmail.com>
2020-09-27 22:30:27 +0200

    adding product legalities dictionnary to cache data on server

---
+++ models/productlegalities.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class productLegalities extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  productLegalities.init(
+    {
+      idProduct: DataTypes.INTEGER,
+      isLegalCommander: DataTypes.INTEGER,
+      isLegalDuel: DataTypes.INTEGER,
+      isLegalLegacy: DataTypes.INTEGER,
+      isLegalModern: DataTypes.INTEGER,
+      isLegalVintage: DataTypes.INTEGER,
+      isLegalPenny: DataTypes.INTEGER,
+      isLegalPauper: DataTypes.INTEGER,
+      isLegalHistoric: DataTypes.INTEGER,
+      isLegalPioneer: DataTypes.INTEGER,
+      isLegalBrawl: DataTypes.INTEGER,
+      isLegalFuture: DataTypes.INTEGER,
+      isLegalStandard: DataTypes.INTEGER,
+      isLegalOldSchool: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "productLegalities",
+    }
+  );
+  return productLegalities;
+};
