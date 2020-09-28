44dbb828adfdbd65b6b5a27da5f010af3c8786bc
Yoann <ybuzenet@gmail.com>
2020-09-28 17:45:50 +0200

    adding properties to priceguide

--- models/priceguide.js
+++ models/priceguide.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class priceguide extends Model {
     /**
@@ -12,12 +10,30 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  priceguide.init({
-    idProduct: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'priceguide',
-  });
+  }
+  priceguide.init(
+    {
+      idProduct: DataTypes.INTEGER,
+      AvgSellPrice: DataTypes.INTEGER,
+      lowPrice: DataTypes.INTEGER,
+      trendPrice: DataTypes.INTEGER,
+      germanProLow: DataTypes.INTEGER,
+      suggestedPrice: DataTypes.INTEGER,
+      foilSell: DataTypes.INTEGER,
+      foilLow: DataTypes.INTEGER,
+      foilTrend: DataTypes.INTEGER,
+      lowPriceEx: DataTypes.INTEGER,
+      avg1: DataTypes.INTEGER,
+      avg7: DataTypes.INTEGER,
+      avg30: DataTypes.INTEGER,
+      foilAvg1: DataTypes.INTEGER,
+      foilAvg7: DataTypes.INTEGER,
+      foilAvg30: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "priceguide",
+    }
+  );
   return priceguide;
 };
