f80747f0dc05f7c2ef9c109ae67dfcb4f762de46
Yoann <ybuzenet@gmail.com>
2020-09-28 18:26:55 +0200

    addind missing dependancies

--- server/server.js
+++ server/server.js
@@ -94,6 +94,10 @@ app.get("/api/updateSetPrice", async (req, res) => {
   res.status(200).json("OK");
 });
 
+app.get("/", (req, res) => {
+  res.status(200).json("HomePage for SSR");
+});
+
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
