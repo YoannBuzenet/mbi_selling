6257fa6231b670d9c67c9be27630505f700cb7e6
Yoann <ybuzenet@gmail.com>
2020-09-29 22:35:04 +0200

    preparing routes to get all mcmId and Legalities asosciated

---
+++ server/controllers/mtgDataController.js
@@ -0,0 +1,5 @@
+const axios = require("axios");
+
+function getAllMcmIdAndLegalities() {}
+
+module.exports = { getAllMcmIdAndLegalities };
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 const mkmRoutes = require("./mkm");
 const dbRoutes = require("./db");
+const mtgDataRoutes = require("./mtgData");
 
 router.use("/mkm", mkmRoutes);
 router.use("/testConnection", dbRoutes);
+router.use("/mtgData", mtgDataRoutes);
 
 module.exports = router;
