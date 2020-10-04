b110a55a8539b5b73520637bced16119daac9513
Yoann <ybuzenet@gmail.com>
2020-10-04 22:33:28 +0200

    endpoint get script by Id works

--- server/routes/api/Entities/script.js
+++ server/routes/api/Entities/script.js
@@ -37,4 +37,45 @@ router.get("/getByUserId", async (req, res) => {
   res.status(200).json(userScripts);
 });
 
+router.get("/getById/:id", async (req, res) => {
+  /* ************************** */
+  /* ****SECURITY & CHECKS**** */
+  /* ************************ */
+
+  securityCheckAPI.checkIsJWTThere(req, res);
+
+  securityCheckAPI.checkQueryParams(req, res, "idUser");
+
+  securityCheckAPI.checkType(req.params.id, "number");
+
+  //Check that the requester is who he sayts he is OR is admin
+  const userHasRightToAccess = await securityCheckAPI.checkIfUserIsThisOneOrAdmin(
+    req.headers.authorization,
+    req.query.idUser
+  );
+  console.log("user has the right to access : ", userHasRightToAccess);
+  if (!userHasRightToAccess) {
+    res.status(401).json("User does not have access do this ressource.");
+    return;
+  }
+
+  /* ********************* */
+  /* *****PROCESS******** */
+  /* ******************* */
+
+  const userScripts = await db.Script.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (userScripts === null) {
+    res.status(404).json("This script does not exist.");
+    return;
+  }
+
+  res.status(200).json(userScripts);
+  return;
+});
+
 module.exports = router;
