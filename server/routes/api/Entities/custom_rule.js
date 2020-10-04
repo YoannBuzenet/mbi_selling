9aed3cc65e67324c5b747b14616cbe72f2b9ccaf
Yoann <ybuzenet@gmail.com>
2020-10-04 11:43:59 +0200

    get custom rules works + factorized and ready to expand

--- server/routes/api/Entities/custom_rule.js
+++ server/routes/api/Entities/custom_rule.js
@@ -3,67 +3,59 @@ var router = express.Router();
 const db = require("../../../../models/index");
 const securityCheckAPI = require("../../../services/securityCheckAPI");
 
-router.get("/", async (req, res) => {
-  /* ************************** */
-  /* *****CHECK PARAMS******** */
-  /* ************************ */
+router.get(
+  "/",
+  async (req, res, next) => {
+    securityCheckAPI.checkIsJWTThere(req, res);
 
-  let jwt = req.headers.authorization;
-  if (jwt === undefined) {
-    res.status(406).json("Auth Header is missing !");
+    securityCheckAPI.checkQueryParams(req, res, ["idUser", "idScript"]);
+
+    /* ************************** */
+    /* *****CHECK RIGHTS******** */
+    /* ************************ */
+
+    //Check that the requester is who he sayts he is OR is admin
+    const userHasRightToAccess = await securityCheckAPI.checkIfUserIsThisOneOrAdmin(
+      req.headers.authorization,
+      req.query.idUser
+    );
+    console.log(userHasRightToAccess);
+    if (!userHasRightToAccess) {
+      res.status(401).json("User does not have access do this ressource.");
+    }
+
+    /* ************************** */
+    /* *********PROCESS********* */
+    /* ************************ */
+
+    //Check that there is a script for this user
+    const isThereAScriptForThisUser = await db.Script.findOne({
+      where: {
+        idShop: req.query.idUser,
+      },
+    });
+
+    if (isThereAScriptForThisUser.length === 0) {
+      res.status(401).json("User does not have access do this ressource.");
+    }
+
+    next();
+  },
+
+  async function (req, res) {
+    /* ********************* */
+    /* *****PROCESS******** */
+    /* ******************* */
+
+    const custom_rules = await db.Custom_Rule.findAll({
+      where: {
+        idScript: idScript,
+      },
+    });
+
+    res.json(custom_rules);
   }
-
-  //idUser en query param
-  let idUser = parseInt(req.query.idUser);
-
-  if (isNaN(idUser)) {
-    res.status(406).json("idUser is missing");
-  }
-
-  //idScript en query param
-  let idScript = parseInt(req.query.idScript);
-
-  if (isNaN(idScript)) {
-    res.status(406).json("idScript is missing");
-  }
-
-  /* ************************** */
-  /* *****CHECK RIGHTS******** */
-  /* ************************ */
-
-  //Check that the requester is who he sayts he is OR is admin
-  const userHasRightToAccess = await securityCheckAPI.checkIfUserIsThisOneOrAdmin(
-    jwt,
-    idUser
-  );
-  console.log(userHasRightToAccess);
-  if (!userHasRightToAccess) {
-    res.status(401).json("User does not have access do this ressource.");
-  }
-
-  //Check that there is a script for this user
-  const isThereAScriptForThisUser = await db.Script.findOne({
-    where: {
-      idShop: idUser,
-    },
-  });
-
-  if (isThereAScriptForThisUser.length === 0) {
-    res.status(401).json("User does not have access do this ressource.");
-  }
-
-  /* ********************* */
-  /* *****PROCESS******** */
-  /* ******************* */
-
-  const custom_rules = await db.Custom_Rule.findAll({
-    where: {
-      idScript: idScript,
-    },
-  });
-
-  res.json(custom_rules);
-});
+);
 
 router.post("/", async (req, res) => {
   //TODO : DO IT !
