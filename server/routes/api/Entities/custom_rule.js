d26427bc6d8c91e2586e4d29caba90924cdc09d8
Yoann <ybuzenet@gmail.com>
2020-10-04 19:25:59 +0200

    PATCH works on custom rules + validation OK !

--- server/routes/api/Entities/custom_rule.js
+++ server/routes/api/Entities/custom_rule.js
@@ -169,11 +169,118 @@ router.post(
   }
 );
 
-router.put("/:id", async (req, res) => {
-  //TODO : DO IT !
-  const jwt = req.headers.authorization;
+router.patch("/:id", async (req, res) => {
+  /* ************************** */
+  /* ****SECURITY & CHECKS**** */
+  /* ************************ */
 
-  res.json("put a custom rule !");
+  securityCheckAPI.checkIsJWTThere(req, res);
+
+  securityCheckAPI.checkQueryParams(req, res, "idUser");
+
+  if (req.params.id === undefined) {
+    res.status(406).json("Custom rule ID is mandatory for updating it.");
+    return;
+  }
+
+  //Check the id of this custom rule exist
+  const existingCustomRule = await db.Custom_Rule.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (existingCustomRule === null) {
+    res
+      .status(406)
+      .json("Custom with id " + req.params.id + "rule could not be found.");
+    return;
+  }
+
+  /* ************************** */
+  /* ******PAYLOAD CHECK****** */
+  /* ************************ */
+  // Payload custom check for this endpoint
+  if (Object.keys(req.body).length === 0) {
+    res.status(406).json("Custom Rules payload is missing.");
+    return;
+  }
+
+  if (req.body.ruleType === undefined) {
+    res.status(406).json("Custom Rules ruleType is mandatory.");
+    return;
+  }
+  if (req.body.behaviour === undefined) {
+    res.status(406).json("Custom Rules behaviour is mandatory.");
+    return;
+  }
+  if (
+    req.body.isForFoils === undefined ||
+    req.body.isForSigned === undefined ||
+    req.body.isForPlaysets === undefined
+  ) {
+    res
+      .status(406)
+      .json(
+        "Custom Rules isForFoils criteria, or isForSigned, or isForPlaysets is missing."
+      );
+    return;
+  }
+  if (req.body.ruleType === "priceRange") {
+    if (
+      req.body.priceRangeFrom === undefined ||
+      req.body.priceRangeTo === undefined ||
+      req.body.priceRangeValueToSet === undefined
+    ) {
+      res
+        .status(406)
+        .json(
+          "For Price Range rules, priceRangeFrom, priceRangeTo, priceRangeValueToSet are mandatory."
+        );
+      return;
+    }
+  }
+
+  //Check that the requester is who he sayts he is OR is admin
+  const userHasRightToAccess = await securityCheckAPI.checkIfUserIsThisOneOrAdmin(
+    req.headers.authorization,
+    req.query.idUser
+  );
+  console.log("has user right to access", userHasRightToAccess);
+  if (!userHasRightToAccess) {
+    res.status(401).json("User does not have access do this ressource.");
+    return;
+  }
+
+  /* ********************* */
+  /* *****PROCESS******** */
+  /* ******************* */
+
+  console.log("in req.body", req.body);
+  //Register Rule
+  //Send it back with its id !
+  existingCustomRule.ruleType = req.body.ruleType;
+  existingCustomRule.priceRangeFrom = req.body.priceRangeFrom;
+  existingCustomRule.priceRangeTo = req.body.priceRangeTo;
+  existingCustomRule.priceRangeValueToSet = req.body.priceRangeValueToSet;
+  existingCustomRule.behaviour = req.body.behaviour;
+  existingCustomRule.priceRangePercentageFromMkm =
+    req.body.priceRangePercentageFromMkm;
+  existingCustomRule.isForFoils = req.body.isForFoils;
+  existingCustomRule.isForSigned = req.body.isForSigned;
+  existingCustomRule.isForPlaysets = req.body.isForPlaysets;
+  existingCustomRule.mkmPriceGuideReference = req.body.mkmPriceGuideReference;
+  existingCustomRule
+    .save()
+    .then((resp) => {
+      console.log("what do we have here ? IN PUT : ", resp);
+      res.status(200).json(resp.dataValues);
+    })
+    .catch((err) => {
+      console.log("didnt work bro", err);
+      res.status(500).json(err);
+      return;
+    });
 });
 
 module.exports = router;
