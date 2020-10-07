5eb15e47177282c818f65fec16db46756681b58b
Yoann <ybuzenet@gmail.com>
2020-10-07 17:48:29 +0200

    improved code with Mui icon

--- src/components/userCoreComponents/CreateMyScript/AddRuleButton.jsx
+++ src/components/userCoreComponents/CreateMyScript/AddRuleButton.jsx
@@ -2,30 +2,18 @@ import React, { useState } from "react";
 import "./createMyScript.css";
 //Mui
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import IconButton from "@material-ui/core/IconButton";
 
 const AddRuleButton = ({ position, FoilOrRegular, handleClick }) => {
-  const [isHover, setIsHover] = useState(false);
-
-  let classCTA;
-  if (isHover) {
-    classCTA = "CTA CTA-hovered";
-  } else {
-    classCTA = "CTA";
-  }
-
-  console.log(isHover);
-
+  console.log(".");
   return (
-    <div
-      className={classCTA}
-      onMouseEnter={(e) => setIsHover(!isHover)}
-      onMouseLeave={(e) => setIsHover(!isHover)}
+    <IconButton
+      onClick={(e) => handleClick(position, FoilOrRegular)}
+      size={"medium"}
+      color="primary"
     >
-      <AddCircleIcon
-        className="add-icon"
-        onClick={(e) => handleClick(position, FoilOrRegular)}
-      />
-    </div>
+      <AddCircleIcon className="add-icon" fontSize="large" color="primary" />
+    </IconButton>
   );
 };
 
