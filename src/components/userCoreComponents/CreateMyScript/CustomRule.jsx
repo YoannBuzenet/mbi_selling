2937fe4ce30377fa7b97e43f3e20d9d20440c246
Yoann <ybuzenet@gmail.com>
2020-10-07 20:47:04 +0200

    animation works, was correlated to keys!

--- src/components/userCoreComponents/CreateMyScript/CustomRule.jsx
+++ src/components/userCoreComponents/CreateMyScript/CustomRule.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddRuleButton from "./AddRuleButton";
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Typography } from "@material-ui/core";
+import Zoom from "@material-ui/core/Zoom";
 
 const CustomRule = ({
   rule,
@@ -11,6 +12,14 @@ const CustomRule = ({
   addACustomRule,
   deleteACustomRule,
 }) => {
+  const [isZoomed, setIsZoomed] = useState(!rule.wasCreatedHere || true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsZoomed(true);
+    }, 200);
+  }, []);
+
   const handleChange = (e) => {
     console.log("update parent");
   };
@@ -22,49 +31,55 @@ const CustomRule = ({
 
   return (
     <>
-      <div className="ruleContainer">
-        <div className="deleteButton">
-          <IconButton onClick={(e) => deleteACustomRule(index, FoilOrRegular)}>
-            <HighlightOffIcon color="secondary" />
-          </IconButton>
-        </div>
-        DE
-        <input
-          className="inputValueNumber"
-          value={typeof rule.from === "number" ? rule.from : ""}
-          name={"from_" + index + "_" + "Regular"}
-          onChange={(e) => handleChange(e)}
-        />
-        A
-        <input
-          className="inputValueNumber"
-          value={typeof rule.from === "number" ? rule.to : ""}
-          name={"to" + index + "_" + "Regular"}
-          onChange={(e) => handleChange(e)}
-        />
-      </div>
-      <div className="belowRule-CTA">
-        {/* Colored Link between rules */}
-        {index !== parentArrayLength - 1 && (
-          <div className="twoBlocksContainer">
-            <div className={classNameFirstDiv}></div>
-            <div className={classNameSecondDiv}></div>
-            <AddRuleButton
-              position={index + 1}
-              FoilOrRegular={FoilOrRegular}
-              handleClick={addACustomRule}
-              classToAdd="onSeparatedDivButton"
+      <Zoom in={isZoomed}>
+        <Typography>
+          <div className="ruleContainer">
+            <div className="deleteButton">
+              <IconButton
+                onClick={(e) => deleteACustomRule(index, FoilOrRegular)}
+              >
+                <HighlightOffIcon color="secondary" />
+              </IconButton>
+            </div>
+            DE
+            <input
+              className="inputValueNumber"
+              value={typeof rule.from === "number" ? rule.from : ""}
+              name={"from_" + index + "_" + "Regular"}
+              onChange={(e) => handleChange(e)}
+            />
+            A
+            <input
+              className="inputValueNumber"
+              value={typeof rule.from === "number" ? rule.to : ""}
+              name={"to" + index + "_" + "Regular"}
+              onChange={(e) => handleChange(e)}
             />
           </div>
-        )}
-        {index === parentArrayLength - 1 && (
-          <AddRuleButton
-            position={index + 1}
-            FoilOrRegular={FoilOrRegular}
-            handleClick={addACustomRule}
-          />
-        )}
-      </div>
+          <div className="belowRule-CTA">
+            {/* Colored Link between rules */}
+            {index !== parentArrayLength - 1 && (
+              <div className="twoBlocksContainer">
+                <div className={classNameFirstDiv}></div>
+                <div className={classNameSecondDiv}></div>
+                <AddRuleButton
+                  position={index + 1}
+                  FoilOrRegular={FoilOrRegular}
+                  handleClick={addACustomRule}
+                  classToAdd="onSeparatedDivButton"
+                />
+              </div>
+            )}
+            {index === parentArrayLength - 1 && (
+              <AddRuleButton
+                position={index + 1}
+                FoilOrRegular={FoilOrRegular}
+                handleClick={addACustomRule}
+              />
+            )}
+          </div>
+        </Typography>
+      </Zoom>
     </>
   );
 };
