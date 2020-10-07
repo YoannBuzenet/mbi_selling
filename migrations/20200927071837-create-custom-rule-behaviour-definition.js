cf02dbdfb144efc66741941a62bc7598d8f3ac75
Yoann <ybuzenet@gmail.com>
2020-10-07 18:58:55 +0200

    corrected DB to add all behaviour custom rule definitions

---
+++ migrations/20200927071837-create-custom-rule-behaviour-definition.js
@@ -0,0 +1,78 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface
+      .createTable("customRule_behaviour_definitions", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(function () {
+        queryInterface.bulkInsert("customRule_behaviour_definitions", [
+          //todo insert all columns of priceguide
+          {
+            name: "roundUp0.5",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp1",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp2",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp5",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp10",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp15",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp20",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp50",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "roundUp100",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ]);
+      });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("customRule_behaviour_definitions");
+  },
+};
