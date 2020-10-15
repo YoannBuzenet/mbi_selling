6ca504f610723726011510878e62f1ae877e3475
Yoann <ybuzenet@gmail.com>
2020-10-15 14:57:19 +0200

    express user data are overloaded into refresh token request too

---
+++ server/routes/authentication/tokenRefresh.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", async (req, res) => {
+  //Checking payload
+  if (req.body.refresh_token === undefined) {
+    res.status(406).json("Parameters are missing for token refresh.");
+    return;
+  }
+
+  try {
+    const refreskTokenOnMTGAPI = await axios.post(
+      process.env.REACT_APP_MTGAPI_URL + "/token/refresh",
+      { refresh_token: req.body.refresh_token }
+    );
+    console.log(refreskTokenOnMTGAPI.data);
+
+    //Getting all scripts for this user and adding them to the API response
+
+    const userScripts = await db.Script.findAll({
+      where: {
+        idShop: refreskTokenOnMTGAPI.data.user.id,
+      },
+    });
+
+    console.log("user scripts to be added :", userScripts);
+
+    const overloadedResponse = { ...refreskTokenOnMTGAPI.data, userScripts };
+
+    res.json(overloadedResponse);
+  } catch (error) {
+    console.log(error);
+    res.status(401).json("Access Denied.");
+    return;
+  }
+});
+
+module.exports = router;
