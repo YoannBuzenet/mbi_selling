da27996f098245bc31d870cdd31fe650336eded9
Yoann <ybuzenet@gmail.com>
2020-10-15 21:53:57 +0200

    first working version of My Scripts page displaying all user scripts

--- src/pages/AllMyScripts.jsx
+++ src/pages/AllMyScripts.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+
+import AuthContext from "../context/authContext";
 
 const AllMyScripts = () => {
-  return <>All my scripts yeah</>;
+  const { authenticationInfos, setAuthenticationInfos } = useContext(
+    AuthContext
+  );
+
+  return (
+    <div className="container all-my-scripts">
+      <h2>My Scripts</h2>
+      <div>
+        <Table>
+          <Thead>
+            <Th></Th>
+          </Thead>
+          <Tbody>
+            {authenticationInfos.shop.userScripts.map((script) => (
+              <Tr>
+                <Td>
+                  <Link to={"/edit-script/" + script.id}>{script.name}</Link>
+                </Td>
+                <Td>Tester</Td>
+                <Td>Lancer</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default AllMyScripts;
