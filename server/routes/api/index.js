0afd52be728400f87e4fc716f6dc4f4bc2217ceb
Yoann <ybuzenet@gmail.com>
2020-10-17 16:13:03 +0200

    format definition endpoint working

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -13,6 +13,7 @@ const scriptRoutes = require("./Entities/script");
 const customrule_behaviour_definition = require("./Entities/customrule_behaviour_definition");
 const customrule_ruletype_definition = require("./Entities/customrule_ruletype_definition");
 const priceguideDefinition = require("./Entities/priceguideDefinition");
+const formatsDefinition = require("./Entities/format");
 
 //Testing routes
 const testRoutes = require("./test");
@@ -37,5 +38,6 @@ router.use("/script", scriptRoutes);
 router.use("/customrule_behaviour_definition", customrule_behaviour_definition);
 router.use("/customrule_ruletype_definition", customrule_ruletype_definition);
 router.use("/priceguideDefinition", priceguideDefinition);
+router.use("/formats", formatsDefinition);
 
 module.exports = router;
