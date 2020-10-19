f78f4c23871d074074dabfcff4674686efd13b18
Yoann <ybuzenet@gmail.com>
2020-10-19 20:09:21 +0200

    settings first table display, css to be done then plug then translate

--- src/components/userCoreComponents/Settings/Settings.jsx
+++ src/components/userCoreComponents/Settings/Settings.jsx
@@ -3,8 +3,7 @@ import "./style.css";
 import Button from "@material-ui/core/Button";
 import { FormattedMessage, useIntl } from "react-intl";
 import { makeStyles } from "@material-ui/core/styles";
-import { Table, Thead, Tbody, Tr, Th } from "react-super-responsive-table";
-import "../super-responsive-table.css";
+import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 
 const Settings = () => {
   const [shouldStateBeSaved, setShouldStateBeSaved] = useState(false);
@@ -49,45 +48,150 @@ const Settings = () => {
           des cartes et de leur langue ?
         </header>
         <div>
-          REGULAR
           <div>
-            <Table>
-              <Thead>
-                <Th>Condition</Th>
-                <Th>Percentage To Apply</Th>
-              </Thead>
-              <Tbody>
-                <Td></Td>
-              </Tbody>
-            </Table>
+            REGULAR
+            <div>
+              <Table className="settings-table regular">
+                <Thead>
+                  <Tr>
+                    <Th>Condition</Th>
+                    <Th>Percentage To Apply</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Mint</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Near-Mint</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Excellent</Td>
+                    <Td>90%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Good</Td>
+                    <Td>80%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Light-Played</Td>
+                    <Td>70%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Played</Td>
+                    <Td>60%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Poor</Td>
+                    <Td>50%</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
           </div>
-        </div>
-        <div>
-          FOIL
           <div>
-            <Table>
-              <Thead>
-                <Th>Condition</Th>
-                <Th>Percentage To Apply</Th>
-              </Thead>
-              <Tbody>
-                <Td></Td>
-              </Tbody>
-            </Table>
+            FOIL
+            <div>
+              <Table className="settings-table foil">
+                <Thead>
+                  <Tr>
+                    <Th>Condition</Th>
+                    <Th>Percentage To Apply</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Mint</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Near-Mint</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Excellent</Td>
+                    <Td>90%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Good</Td>
+                    <Td>80%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Light-Played</Td>
+                    <Td>70%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Played</Td>
+                    <Td>60%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Poor</Td>
+                    <Td>50%</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
           </div>
-        </div>
-        <div>
-          LANGUE
           <div>
-            <Table>
-              <Thead>
-                <Th>Language</Th>
-                <Th>Percentage To Apply</Th>
-              </Thead>
-              <Tbody>
-                <Td></Td>
-              </Tbody>
-            </Table>
+            LANGUE
+            <div>
+              <Table className="settings-table language">
+                <Thead>
+                  <Tr>
+                    <Th>Language</Th>
+                    <Th>Percentage To Apply</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>German</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Spanish</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>French</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Italian</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Japanese</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Portuguese(Brazil)</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Russian</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Chinese Simplified</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>English</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Korean</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Chinese Traditional</Td>
+                    <Td>100%</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </div>
           </div>
         </div>
       </div>
