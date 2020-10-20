cb23fd34943bb8e7272c246e52ddab943016e42e
Yoann <ybuzenet@gmail.com>
2020-10-20 22:18:21 +0200

    calculator for class MKM connexion indicator depending of the timestamp of time left

--- src/components/MKMConnexionIndicator.jsx
+++ src/components/MKMConnexionIndicator.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import "status-indicator/styles.css";
 import AuthContext from "../context/authContext";
 import { FormattedMessage } from "react-intl";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const MKMConnexionIndicator = () => {
   //Current Authentication
@@ -9,13 +11,63 @@ const MKMConnexionIndicator = () => {
     AuthContext
   );
 
+  //State can be : Connected, JustConnected, SoonToUnlog, JustSoonToUnlog, Unlogged, JustUnlogged
+
+  const mkmConnexionStateCalculator = (expirationToken) => {
+    const timeStampOfNow = new Date().getTime();
+    const timeLeftInMilliSeconds = timeStampOfNow - expirationToken;
+    // If timestamp left more than 23h58, it means we are connected since less than 2 minutes
+    if (timeLeftInMilliSeconds > 86280000) {
+      return "JustConnected";
+    }
+    //Between 23h58 and 2 hours - normal connected
+    else if (
+      timeLeftInMilliSeconds < 86280000 &&
+      timeLeftInMilliSeconds > 7200000
+    ) {
+      return "Connected";
+    }
+    // Below 2 hours and 10 minutes left
+    else if (
+      timeLeftInMilliSeconds < 7200000 &&
+      timeLeftInMilliSeconds > 600000
+    ) {
+      return "SoonToUnlog";
+    } else if (timeLeftInMilliSeconds < 600000) {
+      return "JustSoonToUnlog";
+    }
+    //Below 0 and above -2 minutes
+    else if (timeLeftInMilliSeconds < 0 && timeLeftInMilliSeconds > -120000) {
+      return "JustUnlogged";
+    } else {
+      return "Unlogged";
+    }
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
   console.log("auth context in mkm connexion indicator", authenticationInfos);
 
   return (
-    <span>
-      MKM Connection
-      <status-indicator positive pulse></status-indicator>
-    </span>
+    <div className="mkm-connexion-indicator ">
+      <Button variant="outlined" color="default" className={classes.root}>
+        <span className="connexion-text">
+          <FormattedMessage
+            id="mkmConnectionIndicator.title"
+            default="MKM Connection"
+          />
+        </span>
+        <status-indicator positive pulse></status-indicator>
+      </Button>
+    </div>
   );
 };
 
