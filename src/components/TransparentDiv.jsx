dd3f86d3d65ddc3529d4bc4094b1139ee35926c8
Yoann <ybuzenet@gmail.com>
2020-10-21 22:52:58 +0200

    transparent div works and close MKM modal

--- src/components/TransparentDiv.jsx
+++ src/components/TransparentDiv.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import TransparentDivContext from "../context/transparentDivContext";
+import MKM_ModalContext from "../context/mkmModalConnectionContext";
 
 const TransparentDiv = () => {
-  return <>Transparent Div</>;
+  //Transparent Div Context
+  const {
+    isTransparentDivDisplayed,
+    setIsTransparentDivDisplayed,
+  } = useContext(TransparentDivContext);
+
+  //MKM Modal Control
+  const { isMKMModalDisplayed, setIsMKMModalDisplayed } = useContext(
+    MKM_ModalContext
+  );
+
+  const closeEverything = (event) => {
+    setIsTransparentDivDisplayed(false);
+    setIsMKMModalDisplayed(false);
+  };
+
+  return (
+    <div className="transparentDiv" onClick={(e) => closeEverything(e)}>
+      Transparent Div
+    </div>
+  );
 };
 
 export default TransparentDiv;
