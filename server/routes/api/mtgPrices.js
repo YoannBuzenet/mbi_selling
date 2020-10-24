a6104ba805ca85c0024d1251ba2bd282de32d0c9
Yoann <ybuzenet@gmail.com>
2020-10-24 11:11:12 +0200

    Adding control of security level on mtgData and mtgPrices endpoints

--- server/routes/api/mtgPrices.js
+++ server/routes/api/mtgPrices.js
@@ -2,12 +2,29 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
-  //TODO : DO IT !
+  //TODO : AJOUTER CONTROLE
+
   const jwt = req.headers.authorization;
+
+  if (jwt === undefined) {
+    res.status(406).json("Auth Header is missing !");
+  } else {
+    //If there is a token, we remove the "Bearer" part
+    jwt = jwt.split(" ");
+    jwt = jwt[1];
+  }
+
+  // Check 3 : JWT
+  // Auth delegation - checking if the account is a ROLE_ADMIN
+  const isAdmin = await securityCheckAPI.checkIfUserIsAdmin(jwt);
+  if (!isAdmin) {
+    res.status(401).json("You don't have access to this ressource.");
+  }
+
   const { getAllPrices } = require("../../controllers/priceGuideController");
 
   getAllPrices(jwt);
-  res.json("getting MTG prices EN MASSE !");
+  res.json("Price in DB have been updated.");
 });
 
 module.exports = router;
