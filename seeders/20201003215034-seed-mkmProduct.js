102e60b9a2f2bc53832ebd96b288dde43f91ceaf
Yoann <ybuzenet@gmail.com>
2020-10-27 23:12:54 +0100

    passed manyToMany to OneToMany relation

--- seeders/20201003215034-seed-mkmProduct.js
+++ seeders/20201003215034-seed-mkmProduct.js
@@ -5,31 +5,37 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const db = require("../models/index");
 
-    return await db.MkmProduct.bulkCreate([
+    return await db.MkmProduct.bulkCreate(
+      [
+        {
+          idArticle: 1,
+          idProduct: 1,
+          englishName: "ok",
+          localName: "ok",
+          Exp: 5,
+          expName: "ok",
+          price: 5,
+          language: 6,
+          condition: 7,
+          isFoil: 0,
+          isSigned: 0,
+          isPlayset: 0,
+          isAltered: 0,
+          comments: 0,
+          amount: 10,
+          onSale: 0,
+          idCurrency: 1,
+          currencyCode: "EUR",
+          idShop: 4,
+          // productLegalities: [{ idProduct: 1 }],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
       {
-        idArticle: 1767,
-        idProduct: 1,
-        englishName: "ok",
-        localName: "ok",
-        Exp: 5,
-        expName: "ok",
-        price: 5,
-        language: 6,
-        condition: 7,
-        isFoil: 0,
-        isSigned: 0,
-        isPlayset: 0,
-        isAltered: 0,
-        comments: 0,
-        amount: 10,
-        onSale: 0,
-        idCurrency: 1,
-        currencyCode: "EUR",
-        idShop: 4,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+        include: [db.productLegalities],
+      }
+    );
 
     return queryInterface.bulkInsert(
       "MkmProducts",
