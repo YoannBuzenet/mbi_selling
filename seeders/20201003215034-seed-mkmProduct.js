e7602d4a7c3f217ee3277969e400f88efd40e523
Yoann <ybuzenet@gmail.com>
2020-10-27 19:50:17 +0100

    bulkCreate works in seed files

--- seeders/20201003215034-seed-mkmProduct.js
+++ seeders/20201003215034-seed-mkmProduct.js
@@ -4,6 +4,33 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const db = require("../models/index");
+
+    return await db.MkmProduct.bulkCreate([
+      {
+        idArticle: 1767,
+        idProduct: 1,
+        englishName: "ok",
+        localName: "ok",
+        Exp: 5,
+        expName: "ok",
+        price: 5,
+        language: 6,
+        condition: 7,
+        isFoil: 0,
+        isSigned: 0,
+        isPlayset: 0,
+        isAltered: 0,
+        comments: 0,
+        amount: 10,
+        onSale: 0,
+        idCurrency: 1,
+        currencyCode: "EUR",
+        idShop: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+
     return queryInterface.bulkInsert(
       "MkmProducts",
       [
