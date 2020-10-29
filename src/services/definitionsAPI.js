4fdd147b87252628085d97f0e9bff77d1893be83
Yoann <ybuzenet@gmail.com>
2020-10-29 13:38:57 +0100

    script execution finds successfully relevant cards per formats

--- src/services/definitionsAPI.js
+++ src/services/definitionsAPI.js
@@ -34,18 +34,18 @@ function getFormatsDefinitions() {
 }
 
 function getFormatsAndReturnHashtable() {
-  console.log("is this function being called ?");
+  // console.log("is this function being called ?");
   return axios
     .get(process.env.REACT_APP_THIS_WEBSITE_URL + "/api/formats/getDefinitions")
     .then((resp) => {
-      console.log("formats def resp", resp);
+      // console.log("formats def resp", resp);
       let hashmapToreturn = {};
       for (let i = 0; i < resp.data.length; i++) {
         hashmapToreturn[resp.data[i].id] = utils.capitalizeFirstLetter(
           resp.data[i].name
         );
       }
-      console.log("hashtable to return", hashmapToreturn);
+      // console.log("hashtable to return", hashmapToreturn);
       return hashmapToreturn;
     })
     .catch((err) =>
