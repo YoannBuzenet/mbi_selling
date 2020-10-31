a65f825b42274d5772bc02d885285d65cc09b0ba
Yoann <ybuzenet@gmail.com>
2020-10-31 14:11:45 +0100

    snapshot of shop params works !

--- server/services/utils.js
+++ server/services/utils.js
@@ -33,13 +33,53 @@ function prepareStateFromArrayOfRules(arrayOfCustomRules) {
 }
 
 async function snapshotShopParams(idShop) {
-  currentShopParams = await db.shop_params.findOne({
+  const currentShopParams = await db.shop_params.findOne({
     where: {
       idShop: idShop,
     },
   });
-  console.log("les shops params : ", currentShopParams);
-  return currentShopParams;
+  //save current shop param in snapshot
+
+  const snaphotShopParams = await db.snapshot_params.create({
+    shopId: currentShopParams.dataValues.idShop,
+    percentPerSigned: currentShopParams.dataValues.percentPerSigned,
+    percentPerMintRegular: currentShopParams.dataValues.percentPerMintRegular,
+    percentPerNearMintRegular:
+      currentShopParams.dataValues.percentPerNearMintRegular,
+    percentPerExcellentRegular:
+      currentShopParams.dataValues.percentPerExcellentRegular,
+    percentPerGoodRegular: currentShopParams.dataValues.percentPerGoodRegular,
+    percentPerLightPlayedRegular:
+      currentShopParams.dataValues.percentPerLightPlayedRegular,
+    percentPerPlayedRegular:
+      currentShopParams.dataValues.percentPerPlayedRegular,
+    percentPerPoorRegular: currentShopParams.dataValues.percentPerPoorRegular,
+    percentPerMintFoil: currentShopParams.dataValues.percentPerMintFoil,
+    percentPerNearMintFoil: currentShopParams.dataValues.percentPerNearMintFoil,
+    percentPerExcellentFoil:
+      currentShopParams.dataValues.percentPerExcellentFoil,
+    percentPerGoodFoil: currentShopParams.dataValues.percentPerGoodFoil,
+    percentPerLightPlayedFoil:
+      currentShopParams.dataValues.percentPerLightPlayedFoil,
+    percentPerPlayedFoil: currentShopParams.dataValues.percentPerPlayedFoil,
+    percentPerPoorFoil: currentShopParams.dataValues.percentPerPoorFoil,
+    percentPerLangGerman: currentShopParams.dataValues.percentPerLangGerman,
+    percentPerLangSpanish: currentShopParams.dataValues.percentPerLangSpanish,
+    percentPerLangFrench: currentShopParams.dataValues.percentPerLangFrench,
+    percentPerLangItalian: currentShopParams.dataValues.percentPerLangItalian,
+    percentPerLangJapanese: currentShopParams.dataValues.percentPerLangJapanese,
+    percentPerLangPortuguese:
+      currentShopParams.dataValues.percentPerLangPortuguese,
+    percentPerLangRussian: currentShopParams.dataValues.percentPerLangRussian,
+    percentPerLangSimplifiedChinese:
+      currentShopParams.dataValues.percentPerLangSimplifiedChinese,
+    percentPerLangEnglish: currentShopParams.dataValues.percentPerLangEnglish,
+    percentPerLangKorean: currentShopParams.dataValues.percentPerLangKorean,
+    percentPerLangTraditionalChinese:
+      currentShopParams.dataValues.percentPerLangTraditionalChinese,
+  });
+
+  return snaphotShopParams;
 }
 
 module.exports = { prepareStateFromArrayOfRules, snapshotShopParams };
