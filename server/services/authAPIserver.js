752b002f74d4c1934ddba352371e51a2f914825e
Yoann <ybuzenet@gmail.com>
2020-11-07 11:39:49 +0100

    priceguide for each card is being found!

--- server/services/authAPIserver.js
+++ server/services/authAPIserver.js
@@ -4,11 +4,11 @@ async function refreshTokenAndInfos(refresh_token) {
   let object_refresh_token = { refresh_token: refresh_token };
 
   return axios
-    .post("/authentication/token/refresh", object_refresh_token)
+    .post(
+      process.env.REACT_APP_MTGAPI_URL + "/token/refresh",
+      object_refresh_token
+    )
     .then((data) => {
-      //Saving the new JWT token in localStorage
-      window.localStorage.setItem("authToken", data.data.token);
-
       //Puting token into axios bearer
       axios.defaults.headers["Authorization"] = "Bearer " + data.data.token;
 
