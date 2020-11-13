63f4b0ba32322353363f7a2e0cf2ec08d9918eee
Yoann <ybuzenet@gmail.com>
2020-11-13 13:21:25 +0100

    added fullstrack translation folderi n the app for both server and the front to be able to translate price shield behaviour

---
+++ src/services/fullstackTranslations/priceshieldTranslations.js
@@ -0,0 +1,36 @@
+const priceShieldReasons = {
+  1: {
+    "en-US":
+      "Price under 20 euros - Price can't be more than 40% cheaper than the MKM trend.",
+    "fr-FR":
+      "Prix en dessous de 20 euros - Le prix de vente ne peut être inférieur de plus de 40% à la tendance MKM.",
+  },
+  2: {
+    "en-US":
+      "Price between 20 and 50 euros - Price can't be more than 30% cheaper than the MKM trend.",
+    "fr-FR":
+      "Prix entre 20 et 50 euros - Le prix de vente ne peut être inférieur de plus de 30% à la tendance MKM.",
+  },
+  3: {
+    "en-US":
+      "Price above 50 euros - Price can't be more than 20% cheaper than the MKM trend.",
+    "fr-FR":
+      "Prix au dessus de  50 euros - Le prix de vente ne peut être inférieur de plus de 20% à la tendance MKM.",
+  },
+  4: {
+    "en-US":
+      "Price above 10 euros - Variation Rate between old price and new price can't be higher than 40% ",
+    "fr-FR":
+      "Prix au dessus  de 10 euros - Le taux de variation entre l'ancien et le nouveau prix ne peut être supérieur à 40%.",
+  },
+  5: {
+    "en-US":
+      "Variation Rate between old price and new price can't be higher than 60%.",
+    "fr-FR":
+      "Le taux de variation entre l'ancien et le nouveau prix ne peut être supérieur à 60%.",
+  },
+};
+
+module.exports = {
+  priceShieldReasons,
+};
