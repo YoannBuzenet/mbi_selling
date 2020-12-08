974e8b59820c436188224d1e91fac0f57827e746
Yoann <ybuzenet@gmail.com>
2020-12-08 22:32:38 +0100

    added fields to invoices

--- migrations/20201003133152-create-invoice.js
+++ migrations/20201003133152-create-invoice.js
@@ -1,27 +1,53 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Invoices', {
+    await queryInterface.createTable("Invoices", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       idShop: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
+      idInvoice: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      subscribingStartDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      subscribingEndDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      amountTaxIncluded: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      amountTaxExcluded: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      VATSum: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      VATStatus: { type: Sequelize.INTEGER, allowNull: false },
+      stripeReference: { type: Sequelize.STRING, allowNull: false },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Invoices');
-  }
+    await queryInterface.dropTable("Invoices");
+  },
 };
