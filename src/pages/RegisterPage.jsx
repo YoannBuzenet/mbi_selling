a1de8334abbe8be2ab8e5ca04ee93e02d9f5a582
Yoann <ybuzenet@gmail.com>
2020-12-08 21:50:32 +0100

    register page simplified and works with back end

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -15,12 +15,6 @@ const RegisterPage = ({ history }) => {
   const [credentials, setCredentials] = useState({
     mail: "",
     password: "",
-    firstName: "",
-    lastName: "",
-    tel: "",
-    adress: "",
-    postalCode: "",
-    town: "",
   });
 
   const { currentLang, setCurrentLang } = useContext(SelectAppLangContext);
@@ -36,16 +30,7 @@ const RegisterPage = ({ history }) => {
     try {
       const jsonToSend = {
         email: credentials.mail,
-        pass: credentials.password,
-        client: {
-          nom: credentials.lastName,
-          prenom: credentials.firstName,
-          adress: credentials.adress,
-          postalCode: credentials.postalCode,
-          town: credentials.town,
-          tel: credentials.tel,
-          shop: "/shops/" + process.env.REACT_APP_SHOP_ID,
-        },
+        password: credentials.password,
       };
       //Sending the register demand, sending a welcome email if registering was successful
       const serverResponse = await userAPI.register(jsonToSend);
@@ -166,125 +151,6 @@ const RegisterPage = ({ history }) => {
             minlength={6}
           />
 
-          <Field
-            type="text"
-            id="firstName"
-            name="firstName"
-            placeholder={
-              <FormattedMessage
-                id="app.RegisterPage.placeholder.firstName"
-                defaultMessage={`First Name`}
-              />
-            }
-            required
-            onChange={(event) => handleChange(event)}
-            label={
-              <FormattedMessage
-                id="app.RegisterPage.label.firstName"
-                defaultMessage={`First Name`}
-              />
-            }
-          />
-
-          <Field
-            type="text"
-            id="lastName"
-            name="lastName"
-            placeholder={
-              <FormattedMessage
-                id="app.RegisterPage.placeholder.lastName"
-                defaultMessage={`Last Name`}
-              />
-            }
-            required
-            onChange={(event) => handleChange(event)}
-            label={
-              <FormattedMessage
-                id="app.RegisterPage.label.lastName"
-                defaultMessage={`Last Name`}
-              />
-            }
-          />
-
-          <Field
-            type="tel"
-            id="tel"
-            name="tel"
-            placeholder={
-              <FormattedMessage
-                id="app.RegisterPage.placeholder.telephone"
-                defaultMessage={`Telephone`}
-              />
-            }
-            required
-            onChange={(event) => handleChange(event)}
-            label={
-              <FormattedMessage
-                id="app.RegisterPage.label.telephone"
-                defaultMessage={`Telephone`}
-              />
-            }
-          />
-
-          <label htmlFor="adress">
-            <FormattedMessage
-              id="app.RegisterPage.label.adress"
-              defaultMessage={`Adress`}
-            />
-          </label>
-          <textarea
-            className="my-account"
-            name="adress"
-            id="adress"
-            placeholder={intl.formatMessage({
-              id: "app.RegisterPage.placeholder.adress",
-              defaultMessage: `Your adress...`,
-            })}
-            cols="22"
-            rows="3"
-            required
-            onChange={(event) => handleChange(event)}
-          ></textarea>
-
-          <Field
-            name="postalCode"
-            type="text"
-            id="postalCode"
-            placeholder={
-              <FormattedMessage
-                id="app.RegisterPage.placeholder.postalCode"
-                defaultMessage={`Postal Code`}
-              />
-            }
-            required
-            onChange={(event) => handleChange(event)}
-            label={
-              <FormattedMessage
-                id="app.RegisterPage.label.postalCode"
-                defaultMessage={`Postal Code`}
-              />
-            }
-          />
-
-          <Field
-            name="town"
-            type="text"
-            id="town"
-            required
-            placeholder={
-              <FormattedMessage
-                id="app.RegisterPage.placeholder.town"
-                defaultMessage={`Town`}
-              />
-            }
-            onChange={(event) => handleChange(event)}
-            label={
-              <FormattedMessage
-                id="app.RegisterPage.label.town"
-                defaultMessage={`Town`}
-              />
-            }
-          />
           {isLoading && (
             <div className="RegisterLoader">
               <CSSLoaderWaitingSpiral />
