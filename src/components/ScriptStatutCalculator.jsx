882afe700230ad2244ae90ad9d77b88e5a6c2806
Yoann <ybuzenet@gmail.com>
2020-12-09 20:25:43 +0100

    debug on status calculator

--- src/components/ScriptStatutCalculator.jsx
+++ src/components/ScriptStatutCalculator.jsx
@@ -10,36 +10,35 @@ const ScriptStatusCalculator = ({ isScriptRunning, savingState }) => {
         defaultMessage="Running"
       />
     );
-    return;
-  }
-
-  switch (savingState) {
-    case "saving": {
-      status = (
-        <FormattedMessage
-          id="scriptLine.buttons.saving"
-          defaultMessage="Saving..."
-        />
-      );
-      break;
-    }
-    case "saved": {
-      status = (
-        <FormattedMessage
-          id="scriptLine.buttons.saved"
-          defaultMessage="Saved !"
-        />
-      );
-      break;
-    }
-    default: {
-      status = (
-        <FormattedMessage
-          id="scriptLine.buttons.upToDate"
-          defaultMessage="Up to date"
-        />
-      );
-      break;
+  } else {
+    switch (savingState) {
+      case "saving": {
+        status = (
+          <FormattedMessage
+            id="scriptLine.buttons.saving"
+            defaultMessage="Saving..."
+          />
+        );
+        break;
+      }
+      case "saved": {
+        status = (
+          <FormattedMessage
+            id="scriptLine.buttons.saved"
+            defaultMessage="Saved !"
+          />
+        );
+        break;
+      }
+      default: {
+        status = (
+          <FormattedMessage
+            id="scriptLine.buttons.upToDate"
+            defaultMessage="Up to date"
+          />
+        );
+        break;
+      }
     }
   }
 
