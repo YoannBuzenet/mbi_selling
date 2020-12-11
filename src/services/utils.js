a2cd02d933dd0460f421cce87058c276f6ca21a6
Yoann <ybuzenet@gmail.com>
2020-12-11 21:24:01 +0100

    state of script is now updated every 3 minutes on user session thanks to API call

--- src/services/utils.js
+++ src/services/utils.js
@@ -2,4 +2,19 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-module.exports = { capitalizeFirstLetter };
+function transformArrayIntoDictionnaryWithKey(array, key = "id") {
+  var dictionnaryToReturn = {};
+
+  if (array) {
+    for (let i = 0; i < array.length; i++) {
+      dictionnaryToReturn[array[i][key]] = array[i];
+    }
+  }
+
+  return dictionnaryToReturn;
+}
+
+module.exports = {
+  capitalizeFirstLetter,
+  transformArrayIntoDictionnaryWithKey,
+};
