cc3bb0a39a47e3251c69b51119636811e84bc084
Yoann <ybuzenet@gmail.com>
2020-12-12 16:35:19 +0100

    subscribe page css on its way

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -1,12 +1,64 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const Subscribe = () => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div>
       <h1 className="front-title">Choose the best plan for your business</h1>
       <div className="planContainer">
-        <div className="planSubscribing">plan 1</div>
-        <div className="planSubscribing">plan 2</div>
+        <div className="planSubscribing">
+          <p className="price">
+            <span className="number">49</span>
+            <span className="currency">€</span>
+          </p>
+          <p className="explaination">One Month</p>
+          <ul>
+            <li>
+              <p>Unlimited price updates</p>
+            </li>
+            <li>
+              <p>Customize your own scripts</p>
+            </li>
+            <li>
+              <p>PDF summary of each modification</p>
+            </li>
+          </ul>
+          <Button variant="contained" color="primary" size="large">
+            Subscribe
+          </Button>
+        </div>
+        <div className="planSubscribing">
+          <p className="price">
+            <span className="number">29</span>
+            <span className="currency">€</span>
+          </p>
+          <p className="explaination">Three Month</p>
+          <ul>
+            <li>
+              <p>Unlimited price updates</p>
+            </li>
+            <li>
+              <p>Customize your own scripts</p>
+            </li>
+            <li>
+              <p>PDF summary of each modification</p>
+            </li>
+          </ul>
+          <Button variant="contained" color="primary" size="large">
+            Subscribe
+          </Button>
+        </div>
       </div>
     </div>
   );
