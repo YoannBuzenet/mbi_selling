e8811e124e7380cfe9cdaf5f7c4546e44c41afc0
Yoann <ybuzenet@gmail.com>
2020-12-18 18:33:25 +0100

    setting up boilerplate api endpoint for stripe

---
+++ server/controllers/paymentController.js
@@ -0,0 +1,7 @@
+async function getPayment() {
+  console.log("gerring payment...");
+}
+
+module.exports = {
+  getPayment,
+};
---
+++ server/routes/payment/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+//General Routes
+const stripeRoutes = require("./payment");
+
+//Connecting to DB when arriving in
+const { connect } = require("../../../database/connect");
+connect();
+
+router.use("/subscribe", stripeRoutes);
+
+module.exports = router;
