3df764a1f655ef98bbb429ada2262c4cadbd5db1
Yoann <ybuzenet@gmail.com>
2020-12-21 21:25:14 +0100

    function to check if user has fulfilled info for invoices is OK and implemented in invoice

--- src/pages/MyInvoices.jsx
+++ src/pages/MyInvoices.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
-
 import authContext from "../context/authContext";
 import { toast } from "react-toastify";
 import { FormattedMessage } from "react-intl";
 import { Table, Thead, Tbody, Tr, Th } from "react-super-responsive-table";
 import InvoiceLine from "../components/InvoiceLine";
+import userAPI from "../services/userAPI";
 
 const MyInvoices = () => {
   const { authenticationInfos, setAuthenticationInfos } = useContext(
@@ -37,48 +37,57 @@ const MyInvoices = () => {
           defaultMessage="Invoices"
         />
       </h2>
-      <div>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>
-                <FormattedMessage
-                  id="app.invoicePage.invoice.thead.creationDate"
-                  defaultMessage="Created On"
-                />
-              </Th>
-              <Th>
-                <FormattedMessage
-                  id="app.invoicePage.invoice.thead.beginningDate"
-                  defaultMessage="Starts on"
-                />
-              </Th>
-              <Th>
-                <FormattedMessage
-                  id="app.invoicePage.invoice.thead.endingDate"
-                  defaultMessage="Ends On"
-                />
-              </Th>
-              <Th>
-                <FormattedMessage
-                  id="app.invoicePage.invoice.thead.amount"
-                  defaultMessage="Amount"
-                />
-              </Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {listOfInvoices.map((invoice, index) => {
-              return (
-                <>
-                  <InvoiceLine invoice={invoice} />
-                </>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </div>
+      {authenticationInfos &&
+        userAPI.hasGivenMandatoryInformationForInvoices(
+          authenticationInfos
+        ) && (
+          <div>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>
+                    <FormattedMessage
+                      id="app.invoicePage.invoice.thead.creationDate"
+                      defaultMessage="Created On"
+                    />
+                  </Th>
+                  <Th>
+                    <FormattedMessage
+                      id="app.invoicePage.invoice.thead.beginningDate"
+                      defaultMessage="Starts on"
+                    />
+                  </Th>
+                  <Th>
+                    <FormattedMessage
+                      id="app.invoicePage.invoice.thead.endingDate"
+                      defaultMessage="Ends On"
+                    />
+                  </Th>
+                  <Th>
+                    <FormattedMessage
+                      id="app.invoicePage.invoice.thead.amount"
+                      defaultMessage="Amount"
+                    />
+                  </Th>
+                  <Th>PDF</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {listOfInvoices.map((invoice, index) => {
+                  return (
+                    <>
+                      <InvoiceLine invoice={invoice} />
+                    </>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </div>
+        )}
+      {authenticationInfos &&
+        !userAPI.hasGivenMandatoryInformationForInvoices(
+          authenticationInfos
+        ) && <p>Merci de remplir tous les infos CONNARD</p>}
     </div>
   );
 };
