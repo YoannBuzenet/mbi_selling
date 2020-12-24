c76511ffab925880058feb7678798ad2bf037341
Yoann <ybuzenet@gmail.com>
2020-12-24 16:31:59 +0100

    beginning of invoice pdf template

---
+++ src/components/PDFTemplate/InvoiceTemplate.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import config from "../../services/config";
+
+const InvoiceTemplatePDF = ({ shopData, invoiceData }) => {
+  return <Document></Document>;
+};
+
+export default InvoiceTemplatePDF;
