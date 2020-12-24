c7936e358ecc5dc3e770bbbb330dc989eb8b110c
Yoann <ybuzenet@gmail.com>
2020-12-24 17:47:51 +0100

    PDF download works

--- src/components/PDFTemplate/InvoiceTemplate.jsx
+++ src/components/PDFTemplate/InvoiceTemplate.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 import config from "../../services/config";
+import { useIntl, FormattedMessage } from "react-intl";
 
 const InvoiceTemplatePDF = ({ shopData, invoiceData }) => {
+  // Intl hook
+  const intl = useIntl();
+
   return <Document></Document>;
 };
 
