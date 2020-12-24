a4761480ce0214dedbb442481beb07f3acd64418
Yoann <ybuzenet@gmail.com>
2020-12-24 17:47:51 +0100

    PDF download works

--- src/components/InvoiceLine.jsx
+++ src/components/InvoiceLine.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Tr, Td } from "react-super-responsive-table";
-import { FormattedDate, FormattedMessage } from "react-intl";
+import {
+  FormattedDate,
+  FormattedMessage,
+  IntlProvider,
+  useIntl,
+} from "react-intl";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import SelectAppLangContext from "../context/selectedAppLang";
+import AuthContext from "../context/authContext";
+import InvoiceTemplate from "../components/PDFTemplate/InvoiceTemplate";
 
 const InvoiceLine = ({ invoice }) => {
+  //Current Authentication
+  const { authenticationInfos, setAuthenticationInfos } = useContext(
+    AuthContext
+  );
+
+  const intl = useIntl();
+
+  const { currentLang, setCurrentLang } = useContext(SelectAppLangContext);
+
   return (
     <Tr>
       <Td>
@@ -18,10 +36,26 @@ const InvoiceLine = ({ invoice }) => {
       </Td>
       <Td>{invoice.amountTaxIncluded}</Td>
       <Td>
-        <FormattedMessage
-          id="app.invoicePage.invoice.thead.download"
-          defaultMessage="Download"
-        />
+        <PDFDownloadLink
+          document={
+            <IntlProvider
+              locale={currentLang.locale}
+              messages={currentLang.translationsForUsersLocale}
+            >
+              <InvoiceTemplate shopData={authenticationInfos.shop} />
+            </IntlProvider>
+          }
+          fileName={"Invoice n°" + invoice.id + ".pdf"}
+        >
+          {({ blob, url, loading, error }) =>
+            loading
+              ? ""
+              : intl.formatMessage({
+                  id: "app.invoicePage.InvoiceLinegeneratePDF",
+                  defaultMessage: "Download PDF",
+                })
+          }
+        </PDFDownloadLink>
       </Td>
     </Tr>
   );
