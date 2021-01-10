8d2b205d45be91836e608c3b89dd6c6d4505560d
Yoann <ybuzenet@gmail.com>
2021-01-10 11:31:43 +0100

    added default language for the react app to boot on if local storage is empty (was relying on ejs homepag,e but user may skip it as I just did, and get white screen)

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -2,15 +2,26 @@ import React, { useState } from "react";
 import { IntlProvider } from "react-intl";
 import SelectAppLangContext from "../context/selectedAppLang";
 import config from "../services/config";
+import English from "../translations/English.json";
 
 const AppWrapper = (props) => {
   //STATE - App Lang
   //Test that : : fr-FR, en-US, en-GB
   // Finding in the all lang available which has an ID equal to what's in .env
+
+  const langFromLocalStorage = config.websiteDefaultLanguageArrayLangAvailables.filter(
+    (langData) => langData.locale === window.localStorage.getItem("appLang")
+  )?.[0];
+
+  const defaultLanguage = {
+    locale: "en-US",
+    translationsForUsersLocale: English,
+    picture: "EN",
+    langID: 9,
+  };
+
   const [currentLang, setCurrentLang] = useState(
-    ...config.websiteDefaultLanguageArrayLangAvailables.filter(
-      (langData) => langData.locale === window.localStorage.getItem("appLang")
-    )
+    langFromLocalStorage || defaultLanguage
   );
 
   //CONTEXT CREATION
