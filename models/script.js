6537763beda2c8fa04777d0a78032b977735cd4f
Yoann <ybuzenet@gmail.com>
2021-01-15 18:56:46 +0100

    get script now sends back the associated keywords

--- models/script.js
+++ models/script.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Script.hasMany(models.Custom_Rule, { foreignKey: "id" });
-      Script.hasMany(models.Keyword, { foreignKey: "id" });
+      Script.hasMany(models.Keyword, { foreignKey: "idScript" });
       Script.belongsTo(models.User, { foreignKey: "idShop" });
       Script.belongsToMany(models.Format, {
         through: models.ScriptsFormats,
