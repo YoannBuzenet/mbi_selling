67bd2ac7de6e2c11d163ca774680b0a4401e2ece
Yoann <ybuzenet@gmail.com>
2021-01-19 15:47:37 +0100

    first script of automatic script creation for new shop WORKS !

--- server/controllers/shopController.js
+++ server/controllers/shopController.js
@@ -1,7 +1,30 @@
 const db = require("../../models/index");
+const queryInterface = db.sequelize.getQueryInterface();
+const {
+  customRules10percentsFoilStandard,
+} = require("../services/sampleData/shopCreation");
 
-function createPreMadeScriptsForNewUser() {}
+console.log("query interfaaace", queryInterface);
+
+async function createPreMadeScripts10PercentsFoilStandard(idShop) {
+  //-10% standard Foil
+  const newScript = await db.Script.createCustomScript(
+    "-10% Foil Standard",
+    idShop,
+    "oldPrices"
+  );
+
+  // set formats
+  newScript.setFormats([12]);
+
+  // Create custom rules for this script
+  await queryInterface.bulkInsert(
+    "Custom_Rules",
+    customRules10percentsFoilStandard(newScript.dataValues.id),
+    {}
+  );
+}
 
 module.exports = {
-  createPreMadeScriptsForNewUser,
+  createPreMadeScripts10PercentsFoilStandard,
 };
