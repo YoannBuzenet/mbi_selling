0da133e2c979df17ffbb86d300a0a19f7b4acf17
Yoann <ybuzenet@gmail.com>
2021-01-24 12:59:32 +0100

    missing case sensitive problem

--- migrations/20200926064920-create-order.js
+++ migrations/20200926064920-create-order.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "Customer",
+          model: "Customers",
           key: "idCustomerMKM",
         },
       },
