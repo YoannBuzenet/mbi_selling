b2da0ffdb55ee5038cf365e052f6288597e57865
Yoann <ybuzenet@gmail.com>
2021-01-29 22:45:31 +0100

    adding edge case management of null into subscribe check function

--- src/services/subscribeAPI.js
+++ src/services/subscribeAPI.js
@@ -1,5 +1,8 @@
 // Awaited date format is "02-26-2012" (english format without hours/m/seconds corresponding to our column in DB, sequelize type : DateOnly)
 function isUserSubscribed(dateString) {
+  if (dateString === null) {
+    return false;
+  }
   const myDate = dateString.split("-");
 
   const endSubscribingDate = new Date(
