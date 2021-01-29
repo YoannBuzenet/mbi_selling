e3f351e19f3033b03e3bb35c797f71f9c4ba3648
Yoann <ybuzenet@gmail.com>
2021-01-29 22:00:09 +0100

    import Mui style for popin button

--- src/components/PopInLaunchingConfirmation.jsx
+++ src/components/PopInLaunchingConfirmation.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import PopInLaunchingConfirmationContext from "../context/popInConfirmationLaunchingScript";
 import AuthContext from "../context/authContext";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const PopInLaunchingConfirmation = () => {
   const { setPopInLaunchingScriptInformations } = useContext(
@@ -15,13 +17,52 @@ const PopInLaunchingConfirmation = () => {
     console.log("launching script...");
   };
 
+  const useStyles = makeStyles((theme) => ({
+    confirmButton: {
+      backgroundColor: "rgb(0, 177, 106)",
+      "&:hover": {
+        background: "rgb(123, 239, 178)",
+      },
+    },
+    cancelButton: {
+      backgroundColor: "rgb(247, 202, 24)",
+      "&:hover": {
+        background: "rgb(250, 216, 89)",
+      },
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div className="launchingPopInConfirmation">
       u sure ?
-      <button
-        type="button"
-        onClick={(e) => setPopInLaunchingScriptInformations("todo pass object")}
-      ></button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.confirmButton}
+        size="large"
+        onClick={(e) => console.log("launching script then closing window")}
+      >
+        <FormattedMessage
+          id="app.popIn.confirmationLaunching.button.launch"
+          defaultMessage="Launch"
+        />
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.cancelButton}
+        size="large"
+        onClick={(e) =>
+          setPopInLaunchingScriptInformations({ isDisplayed: false })
+        }
+      >
+        <FormattedMessage
+          id="app.popIn.confirmationLaunching.button.cancel"
+          defaultMessage="Cancel"
+        />
+      </Button>
     </div>
   );
 };
