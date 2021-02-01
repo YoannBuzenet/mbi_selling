b759772b2ac096341a4b93158ccd5b4104247a57
Yoann <ybuzenet@gmail.com>
2021-02-01 11:26:32 +0100

    adding verification of env variable in CLI scripts + preparing an edge case on script controller

--- server/CLI-scripts/getMTGdata.js
+++ server/CLI-scripts/getMTGdata.js
@@ -3,6 +3,19 @@ const { getAllFormatDefinition } = require("../controllers/mtgDataController");
 const axios = require("axios");
 
 async function getMTGData() {
+  if (process.env.LOG_SHOP_ADMIN === undefined) {
+    throw new Error(
+      "LOG_SHOP_ADMIN not defined in env var. Did you define it in the OS ?"
+    );
+  } else if (process.env.LOG_SHOP_PWD === undefined) {
+    throw new Error(
+      "LOG_SHOP_PWD not defined in env var. Did you define it in the OS ?"
+    );
+  } else if (process.env.DATABASE_URL_DEV === undefined) {
+    throw new Error(
+      "DATABASE_URL_DEV not defined in env var. Did you define it in the OS ?"
+    );
+  }
   const credentials = {
     email: process.env.LOG_SHOP_ADMIN,
     password: process.env.LOG_SHOP_PWD,
