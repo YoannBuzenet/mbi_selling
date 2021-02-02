e8b596eebd03835773f1ad26373599e5743eb1b3
Yoann <ybuzenet@gmail.com>
2021-02-02 20:00:59 +0100

    corrected variable access thanks to test + beginning gathering data for test script

--- server/__tests__/scripts.js
+++ server/__tests__/scripts.js
@@ -1,4 +1,16 @@
-it("sums numbers", () => {
+const { startScript } = require("../controllers/scriptController");
+
+// is needed
+// idShop, idScript, isTest, shopData, locale, formats, jwt;
+
+// This data has been arbitrarily defined in the seed files
+const idShopTest = 7;
+const idScriptTest = 3;
+const isTest = false;
+//shopData ?
+
+it("starts a script", () => {
+  console.log("ouah :O", startScript);
   console.log("env is : " + process.env.NODE_ENV);
   expect(1 + 2).toEqual(3);
 });
