a517c8090106cd7a7d8a5bbfe1459fe86b81cb9d
Yoann <ybuzenet@gmail.com>
2021-02-06 17:33:43 +0100

    removed the behaviour of refreshing stock in test script in oldPrice if NODE_ENV is test, to avoid deleting the mkmproducts seeds

--- server/controllers/scriptController.js
+++ server/controllers/scriptController.js
@@ -134,7 +134,8 @@ async function startScript(
   if (
     oneCardAtRandomFromStock === null ||
     currentUser.dataValues.shouldHaveStockDataRefreshed === 1 ||
-    scriptData.dataValues.willBeBasedOn === "oldPrices" ||
+    (scriptData.dataValues.willBeBasedOn === "oldPrices" &&
+      process.env.NODE_ENV !== "test") ||
     new Date(oneCardAtRandomFromStock.updatedAt).getTime() +
       parseInt(process.env.TIME_TO_EXPIRE_STOCK, 10) <
       new Date().getTime()
