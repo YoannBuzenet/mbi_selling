1387b0fedc500d0c1f48e44f7ee799e0ab2a1b38
Yoann <ybuzenet@gmail.com>
2021-02-13 20:57:18 +0100

    adding the generation of prebuilt rarities in migration

--- migrations/20201003133153-create-rarity.js
+++ migrations/20201003133153-create-rarity.js
@@ -1,33 +1,58 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("rarities", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      idScript: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Scripts",
-          key: "id",
+    await queryInterface
+      .createTable("rarities", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+        idScript: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Scripts",
+            key: "id",
+          },
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(function () {
+        queryInterface.bulkInsert("rarities", [
+          {
+            name: "rare",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "uncommon",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "common",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            name: "mythic",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ]);
+      });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable("rarities");
